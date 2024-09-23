86a427e198e39bdce6bbe1ee757ba35884b782b4
moyal <shalom14294@gmail.com>
2024-09-23 20:37:24 +0300

    add componenet to page

---
+++ src/app/proposal/page.tsx
@@ -0,0 +1,10 @@
+import { ProposalForm } from "../_components/ProposalForm/ProposalForm";
+import AllProposals from "../_components/AllPropsal/AllPropsal";
+export default function ProposalPage(){
+    return(
+        <> 
+         <ProposalForm />
+         <AllProposals />
+        </>
+    )
+}
